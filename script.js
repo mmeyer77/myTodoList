c2960dd11fbf8c3551ff0376dde2d8b9bf3b3e58
mmeyer77 <59625761+mmeyer77@users.noreply.github.com>
2020-02-11 09:42:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -180,7 +180,7 @@ var view = {
             var deleteButton = document.createElement('button');
             deleteButton.textContent = 'Delete';
             deleteButton.className = 'deleteButton' 
-            deleButton.onClick = 'handlers.deleteStudent(this.parentNode.id)';
+            deleteButton.onClick = 'handlers.deleteStudent(this.parentNode.id)';
             return deleteButton;
         },
 
